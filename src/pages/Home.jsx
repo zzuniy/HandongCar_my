4ffd82fee2fe657e17d4acc4b47e4b70c7e453df
zzyniy <heojueun050930@naver.com>
2025-10-17 20:32:27 +0900

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,11 @@ import { GlobalStyle } from "../assets/styles/StyledComponents";
 function Home(){
   return(
     <>
-      <h1>Home Page</h1>
+      <h2>카풀 모집</h2>
+      <p>함께 이용할 동승자를 찾아보세요</p>
+      <button>게시글 추가</button>
+      <br/>
+     <input type="text"></input>
     </>
   );
 };
