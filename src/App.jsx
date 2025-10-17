fd54d754769565f748727a8c86ee6bf4d0f1397c
playoh <wazoski05@gmail.com>
2025-10-17 14:55:23 +0900

    add: file 초기 설정 및 router path 정의

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import detailPage from './pages/detail';
+import createPage from './pages/create';
+import updatePage from './pages/update';
 import MainLayout from './Layouts/MainLayout';
 
 export default function App() {
@@ -9,6 +12,10 @@ export default function App() {
         <Routes>
           <Route element={<MainLayout/>}> 
             <Route path="/" element={<Home />} />
+            <Route path="/detail" element={<detailPage />} />
+            <Route path="/create" element={<createPage />} />
+            <Route path="/update" element={<updatePage />} />
+            
           </Route>
         </Routes>
     </BrowserRouter>
