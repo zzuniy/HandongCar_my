7d7f88a0e38a1b32e63bad9cebb6eaee1b2e237d
zzyniy <heojueun050930@naver.com>
2025-10-17 21:32:56 +0900

    oss_teample

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,32 @@ import React, {useEffect , useState } from "react";
 import styled from "styled-components";
 import { GlobalStyle } from "../assets/styles/StyledComponents";
 
+const data = [
+  {
+    출발: "포항역",
+    도착: "한동대",
+    member:1,
+    status: "모집중",
+    date: "2025-10-17",
+    phone: "010-1234-5678"
+  },
+  {
+    출발: "한동대",
+    도착: "경주역",
+    member:4,
+    status: "모집종료",
+    date: "2025-10-18",
+    phone: "010-9876-5432"
+  },
+  {
+    출발: "대구공항",
+    도착: "한동대",
+    member:3,
+    status: "모집중",
+    date: "2025-10-19",
+    phone: "010-1111-2222"
+  }
+];
 
 function Home(){
   return(
@@ -11,6 +37,25 @@ function Home(){
       <button>게시글 추가</button>
       <br/>
      <input type="text"></input>
+     <Card />
+    </>
+  );
+};
+
+function Card(){
+  return(
+    <>
+      <div>
+        {data.map((item, index) => (
+          <div key={index} className="card">
+            <p>{item.출발}</p>
+            <p>to{item.도착}</p>
+            <p>{item.member}/4</p>
+            <p>{item.date}</p>
+            <p>{item.phone}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
