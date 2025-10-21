52750b4247b2f0c80a2430e845396a3375f25734
zzyniy <heojueun050930@naver.com>
2025-10-21 19:24:18 +0900

    footer

---
+++ src/Layouts/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2025 HandongCar. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
