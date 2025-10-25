c7c4740f987868281ac79bde9e43d35d4620c7e2
zzyniy <heojueun050930@naver.com>
2025-10-25 20:50:06 +0900

    all

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// ⚠️ Add a parameter 'autoload=false' and a 'callback'
 const script = document.createElement('script');
 script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_APP_KEY}&libraries=services&autoload=false`; 
 
 script.onload = () => {
-  // ⭐️ This is the new, crucial part: Manually bootstrap the map API
+
   window.kakao.maps.load(() => {
     const root = ReactDOM.createRoot(document.getElementById('root'));
     root.render(
