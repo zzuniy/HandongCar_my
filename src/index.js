8c883905a6f1d7d83e6d714387562aa353d98773
zzyniy <heojueun050930@naver.com>
2025-10-25 20:48:58 +0900

    all

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-// const script = document.createElement('script');
-// script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_APP_KEY}&libraries=services`;
-
-
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
