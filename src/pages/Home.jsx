db29241ffed2bfecb5710bd4af45a8a734545a64
playoh <wazoski05@gmail.com>
2025-10-25 21:38:00 +0900

    style: 괄호miss

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { GlobalStyle } from "../assets/styles/StyledComponents";
 
 import './Home.css';
 import { FaEdit, FaTrash, FaMapMarkerAlt, FaRegClock, FaUserFriends, FaPhoneAlt } from "react-icons/fa";
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";
 
 function Home() {
   const [data, setData] = useState([]);
@@ -16,37 +16,35 @@ function Home() {
       .catch((err) => console.error("API 불러오기 오류:", err));
   }, []);
 
-  return(
-    <><div className="layout">
-    <p className="survice-p">한동대학교 전용 서비스</p>
-   <h1 className = "forHGU-h1">한동대학교 학생들을 위한 </h1>
-   <h1 className ="smart-h1">스마트 카풀 매칭</h1>
-   <p className="explain-p">기존 오픈채팅방의 불편함을 해소하고, 직관적인 인터페이스로 원하는 시간대·노선의 카풀을 쉽고 빠르게 찾아보세요.</p>
-   <button className="apply-btn">바로 체험하기</button>
-   </div>
-    </>
-  );
-};
-
-=======
   return (
-    <>
-      <div className="recruit-text">
-        <h1>카풀 모집</h1>
-        <p className="with-text">함께 이용할 동승자를 찾아보세요</p>
-      </div>
-      <div className="addallign">
-        <input type="text" className="search" placeholder="검색"></input>
-        {/* ✅ create 페이지로 이동 */}
-        <Link to="/create" className="addcar as-button">+ 게시글 추가</Link>
-      </div>
-
-      <br />
-
-      <Card data={data} />
+    <><div className="layout">
+      <p className="survice-p">한동대학교 전용 서비스</p>
+      <h1 className="forHGU-h1">한동대학교 학생들을 위한 </h1>
+      <h1 className="smart-h1">스마트 카풀 매칭</h1>
+      <p className="explain-p">기존 오픈채팅방의 불편함을 해소하고, 직관적인 인터페이스로 원하는 시간대·노선의 카풀을 쉽고 빠르게 찾아보세요.</p>
+      <button className="apply-btn">바로 체험하기</button>
+    </div>
     </>
   );
 };
+
+return (
+  <>
+    <div className="recruit-text">
+      <h1>카풀 모집</h1>
+      <p className="with-text">함께 이용할 동승자를 찾아보세요</p>
+    </div>
+    <div className="addallign">
+      <input type="text" className="search" placeholder="검색"></input>
+      {/* ✅ create 페이지로 이동 */}
+      <Link to="/create" className="addcar as-button">+ 게시글 추가</Link>
+    </div>
+
+    <br />
+
+    <Card data={data} />
+  </>
+);
 
 function Card({ data }) {
   if (!Array.isArray(data)) return null;
