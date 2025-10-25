c2756ffd5b34d6c7e05ccd7f261999218086a13d
zzyniy <heojueun050930@naver.com>
2025-10-25 20:48:00 +0900

    index

--- src/index.js
+++ src/index.js
@@ -7,22 +7,7 @@
 // script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_APP_KEY}&libraries=services`;
 
 
-// script.onload = () => {
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-//   root.render(
-//     <React.StrictMode>
-//       <App />
-//     </React.StrictMode>
-//   );
-// }
-// document.head.appendChild(script);
 
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
-// index.js (Original)
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
